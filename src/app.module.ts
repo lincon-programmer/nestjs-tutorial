3acbf6a41a7b3ae6d026d68e5afe4629c21b0ffb
lincon-programmer <lincongallo@gmail.com>
2021-06-09 16:26:58 -0300

    feat: implementation of registration in the database with sequelize

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,28 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { SequelizeModule } from '@nestjs/sequelize/dist/sequelize.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ProdutosController } from './produtos.controller';
+import { Produto } from './produtos/produto.model';
+import { ProdutosController } from './produtos/produtos.controller';
+import { ProdutosService } from './produtos/produtos.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    SequelizeModule.forRoot({
+      dialect: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: process.env.USUARIO_DB,
+      password: process.env.SENHA_DB,
+      database: 'livraria',
+      autoLoadModels: true,
+      synchronize: true,
+    }),
+    SequelizeModule.forFeature([Produto])
+  ],
   controllers: [AppController, ProdutosController],
-  providers: [AppService],
+  providers: [AppService, ProdutosService],
 })
 export class AppModule {}
---
+++ src/produtos/produtos.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from "@nestjs/common";
+import { InjectModel } from "@nestjs/sequelize";
+import { Produto } from "./produto.model";
+
+@Injectable()
+export class ProdutosService {
+    constructor(
+        @InjectModel(Produto)
+        private produtoModel: typeof Produto
+    ){}
+
+    async obterTodos(): Promise<Produto[]> {
+        return this.produtoModel.findAll();
+    }
+
+    async obterUm(id: number): Promise<Produto> {
+        return this.produtoModel.findByPk();
+    }
+
+    async criar(produto: Produto){
+        this.produtoModel.create(produto)
+    }
+
+    async alterar(produto: Produto): Promise<[number, Produto[]]>{
+        return this.produtoModel.update(produto,{
+            where: {
+                id: produto.id
+            }
+        })
+    }
+
+    async apagar(id: number){
+        const produto: Produto = await this.obterUm(id);
+        produto.destroy();
+    }
+}
