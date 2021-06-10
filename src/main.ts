8bc3907c062702dacc12703c42eafd68146034a2
lincon-programmer <lincongallo@gmail.com>
2021-06-10 08:07:20 -0300

    feat: implemented swagger documentation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('produtos-api')
+    .setDescription('this is an api template for product registration, built in nestjs')
+    .setVersion('0.5.0')
+    .build()
+
+  const document = SwaggerModule.createDocument(app, config)
+
+  SwaggerModule.setup('swagger', app, document)
+
   await app.listen(3000);
 }
 bootstrap();
--- src/produtos/produto.model.ts
+++ src/produtos/produto.model.ts
@@ -1,20 +1,24 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { Table, Model, Column, DataType } from "sequelize-typescript";
 
 @Table
 export class Produto extends Model<Produto>{
     
+    @ApiProperty()
     @Column({
         type: DataType.STRING(60),
         allowNull: false,
     })
     codigo: string;
     
+    @ApiProperty()
     @Column({
         type: DataType.STRING,
         allowNull: false,
     })
     nome: string;
 
+    @ApiProperty()
     @Column({
         type: DataType.DECIMAL(10, 2),
         allowNull: false,
